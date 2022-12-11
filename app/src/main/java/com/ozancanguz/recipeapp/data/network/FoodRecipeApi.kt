b94316039047b210d0d9823bf3233056bbda5b41
Ozan <ozancangu24@gmail.com>
2022-12-11 17:38:42 +0300

    -food recipe api interface created -constanst created

---
+++ app/src/main/java/com/ozancanguz/recipeapp/data/network/FoodRecipeApi.kt
@@ -0,0 +1,16 @@
+package com.ozancanguz.recipeapp.data.network
+
+import com.ozancanguz.recipeapp.data.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipeApi {
+
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+}
---
+++ app/src/main/java/com/ozancanguz/recipeapp/utils/constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.ozancanguz.recipeapp.utils.constants
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "97dd239ca98e45d38e09b1982f6552c6"
+
+    }
+
+}
