a070ad03f69c49c904e4c92c9eddbdc830dea413
Ozan <ozancangu24@gmail.com>
2022-12-11 17:58:17 +0300

    -hasInternetConnection fun created

--- app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.ozancanguz.recipeapp.viewmodels
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
 import com.ozancanguz.recipeapp.data.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,7 +13,20 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(repository: Repository,application: Application):AndroidViewModel(application) {
 
 
-
+    // 1
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
 
 
 
