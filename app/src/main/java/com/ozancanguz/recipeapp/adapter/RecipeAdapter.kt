05299a588db11ab902d67abf31e5237d916746a9
Ozan <ozancangu24@gmail.com>
2022-12-12 01:24:47 +0300

    -project structured edited

--- app/src/main/java/com/ozancanguz/recipeapp/adapter/RecipeAdapter.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/adapter/RecipeAdapter.kt
@@ -1,6 +1,5 @@
-package com.ozancanguz.adapter
+package com.ozancanguz.recipeapp.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
