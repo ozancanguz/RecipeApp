c52d0ae5e1cd3816d3a34ecc084ede8ed33327ee
Ozan <ozancangu24@gmail.com>
2022-12-12 00:08:42 +0300

    -RecipeTypeConverter created

---
+++ app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeTypeConverter.kt
@@ -0,0 +1,23 @@
+package com.ozancanguz.recipeapp.data.models.db
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.ozancanguz.recipeapp.data.models.FoodRecipe
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
