f1dcf8642507cc92e3a82bf9a459a917678688ab
Ozan <ozancangu24@gmail.com>
2022-12-11 16:46:09 +0300

    -bottom navigation setup created

--- app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/favorite/FavoriteRecipes.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/favorite/FavoriteRecipes.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import com.ozancanguz.recipeapp.R
 
 
-class FavoriteFragment : Fragment() {
+class FavoriteRecipes : Fragment() {
 
 
     override fun onCreateView(
@@ -16,7 +16,7 @@ class FavoriteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
     }
 
 
--- app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/recipe/Recipes_fragment.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/recipe/Recipes_fragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.ozancanguz.recipeapp.R
-import kotlinx.android.synthetic.main.fragment_recipe.view.*
 
 
-class RecipeFragment : Fragment() {
+class Recipes_fragment : Fragment() {
 
 
     override fun onCreateView(
@@ -18,11 +16,8 @@ class RecipeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_recipe, container, false)
+        val view= inflater.inflate(R.layout.fragment_recipes, container, false)
 
-        view.button.setOnClickListener {
-            findNavController().navigate(R.id.action_recipeFragment_to_favoriteFragment)
-        }
 
     return view
     }
