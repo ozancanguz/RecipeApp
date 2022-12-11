6cb236c69466ac4a111642ca030a537490e171cb
Ozan <ozancangu24@gmail.com>
2022-12-11 17:59:29 +0300

    -getRecipes fun created

--- app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
@@ -5,12 +5,25 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.ozancanguz.recipeapp.data.Repository
+import com.ozancanguz.recipeapp.data.models.FoodRecipe
+import com.ozancanguz.recipeapp.utils.NetworkResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(repository: Repository,application: Application):AndroidViewModel(application) {
+
+
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+
+    fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
+
+    }
+
 
 
     // 1
