311acbd172194334a69f63dfd5ac2825cc37a4f2
Ozan <ozancangu24@gmail.com>
2022-12-12 00:10:42 +0300

    -Recipe database created

---
+++ app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeDatabase.kt
@@ -0,0 +1,18 @@
+package com.ozancanguz.recipeapp.data.models.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ozancanguz.recipeapp.data.models.db.entities.RecipesEntity
+
+@Database(
+    entities = [RecipesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipeDatabase: RoomDatabase() {
+
+    abstract fun recipeDao(): RecipeDao
+
+}
