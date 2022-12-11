84275682b0e54be88e7d2e67bf66b7356972c69e
Ozan <ozancangu24@gmail.com>
2022-12-11 17:47:24 +0300

    -repository created and injected

---
+++ app/src/main/java/com/ozancanguz/recipeapp/data/Repository.kt
@@ -0,0 +1,12 @@
+package com.ozancanguz.recipeapp.data
+
+import com.ozancanguz.recipeapp.data.remote.RemoteDataSource
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class Repository @Inject constructor(private val remoteDataSource: RemoteDataSource) {
+
+
+    val remote=remoteDataSource
+}
