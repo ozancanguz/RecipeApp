c10a626734b2f644106ba96279a170cb7334cd03
Ozan <ozancangu24@gmail.com>
2022-12-12 12:45:18 +0300

    -datastore applied in RecipesBottomSheet class.

--- app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/recipe/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/recipe/bottomsheet/RecipesBottomSheet.kt
@@ -1,20 +1,41 @@
 package com.ozancanguz.recipeapp.ui.fragments.recipe.bottomsheet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.ozancanguz.recipeapp.R
 
 import com.ozancanguz.recipeapp.databinding.RecipesBottomSheetBinding
+import com.ozancanguz.recipeapp.utils.constants.Constants.Companion.DEFAULT_DIET_TYPE
+import com.ozancanguz.recipeapp.utils.constants.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.ozancanguz.recipeapp.viewmodels.MainViewModel
+import com.ozancanguz.recipeapp.viewmodels.RecipeViewModel
+import java.util.*
 
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
+
     private var _binding: RecipesBottomSheetBinding? = null
 
     private val binding get() = _binding!!
+
+
+    //init viewmodel
+    private val recipesViewModel:RecipeViewModel by viewModels()
+
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,14 +44,56 @@ class RecipesBottomSheet : BottomSheetDialogFragment() {
         _binding = RecipesBottomSheetBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner, { value ->
+            mealTypeChip = value.selectedMealType
+            dietTypeChip = value.selectedDietType
+            updateChip(value.selectedMealTypeId, binding.mealTypeChipGroup)
+            updateChip(value.selectedDietTypeId, binding.dietTypeChipGroup)
+        })
 
+        binding.mealTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedMealType = chip.text.toString().toLowerCase(Locale.ROOT)
+            mealTypeChip = selectedMealType
+            mealTypeChipId = selectedChipId
+        }
 
+        binding.dietTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedDietType = chip.text.toString().toLowerCase(Locale.ROOT)
+            dietTypeChip = selectedDietType
+            dietTypeChipId = selectedChipId
+        }
+
+        binding.applyBtn.setOnClickListener {
+            recipesViewModel.saveMealAndDietType(
+                mealTypeChip,
+                mealTypeChipId,
+                dietTypeChip,
+                dietTypeChipId
+            )
+
+        }
 
 
 
 
         return view
     }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
+    }
+
+
+
+
 
 
 }
