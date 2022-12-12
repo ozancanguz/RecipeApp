860550edf986b02a49d042407d812ade6ac0e0d7
Ozan <ozancangu24@gmail.com>
2022-12-12 21:06:00 +0300

    -recipe dao coded for favorite entity

--- app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeDao.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeDao.kt
@@ -1,23 +1,40 @@
 package com.ozancanguz.recipeapp.data.models.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.ozancanguz.recipeapp.data.models.db.entities.FavoriteEntity
 import com.ozancanguz.recipeapp.data.models.db.entities.RecipesEntity
+import com.ozancanguz.recipeapp.ui.fragments.favorite.FavoriteRecipes
 import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface RecipeDao {
 
-    // insert data to dao
+    // insert recipe data to dao
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertData(recipesEntity: RecipesEntity)
 
-    //get all data ref
+    // insert favorite recipes to dao
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoriteEntity)
+
+
+    //get all recipes ref
    @Query("select * from recipes_table order by id ASC")
    fun readRecipes(): Flow<List<RecipesEntity>>
+
+
+   // get all favorite recipes
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoriteEntity>>
+
+    // delete single favorite recipe
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoriteRecipes)
+
+    // delete all fav recipe
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 
 
 
