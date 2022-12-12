baec42ced6fffa54b127e3982d9a482fda5cd707
Ozan <ozancangu24@gmail.com>
2022-12-12 17:24:19 +0300

    -search recipe created in mainviewmodel

--- app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/viewmodels/MainViewModel.kt
@@ -43,11 +43,23 @@ class MainViewModel @Inject constructor(
 
 
     //       -----------------FOR RETROFİT----------------
+    // recipe response
     var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
 
+    // search recipe response
+    var searchedRecipesResponse:MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+
+
+    // get recipes in background thread
     fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
           getRecipesSafeCall(queries)
     }
+
+    // search recipes in background thread
+    fun searchRecipes(searchQuery:Map<String,String>) = viewModelScope.launch {
+        searchRecipesSafeCall(searchQuery)
+    }
+
 
     private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
         recipesResponse.value = NetworkResult.Loading()
@@ -68,6 +80,22 @@ class MainViewModel @Inject constructor(
             recipesResponse.value = NetworkResult.Error("No Internet Connection.")
         }
     }
+    private suspend fun searchRecipesSafeCall(searchQuery: Map<String, String>) {
+        searchedRecipesResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.searchRecipes(searchQuery)
+                recipesResponse.value = handleFoodRecipesResponse(response)
+            } catch (e: Exception) {
+                searchedRecipesResponse.value = NetworkResult.Error("Recipes not found.")
+            }
+        } else {
+            searchedRecipesResponse.value = NetworkResult.Error("No Internet Connection.")
+        }
+    }
+
+
+
 
     // offline cache recipe part 2
     private fun offlineCacheRecipes(foodRecipe: FoodRecipe) {
