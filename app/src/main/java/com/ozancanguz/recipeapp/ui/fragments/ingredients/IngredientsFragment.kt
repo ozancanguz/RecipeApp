951d9b4947bb5723612ed33fc70f430fa6e1a411
Ozan <ozancangu24@gmail.com>
2022-12-12 18:51:27 +0300

    -data displayed in IngredientsFragment.kt

--- app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/ui/fragments/ingredients/IngredientsFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ozancanguz.recipeapp.R
+import com.ozancanguz.recipeapp.adapter.IngredientsAdapter
+import com.ozancanguz.recipeapp.data.models.Result
 import com.ozancanguz.recipeapp.databinding.FragmentIngredientsBinding
+import kotlinx.android.synthetic.main.fragment_ingredients.view.*
 
 
 class IngredientsFragment : Fragment() {
@@ -15,7 +19,7 @@ class IngredientsFragment : Fragment() {
     // This property is only valid between onCreateView and
 // onDestroyView.
     private val binding get() = _binding!!
-
+    private var  ingredientsAdapter=IngredientsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +28,26 @@ class IngredientsFragment : Fragment() {
         _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        setupRv(view)
 
+
+        val args=arguments
+        val myBundle: com.ozancanguz.recipeapp.data.models.Result? = args?.getParcelable<Result>("recipeBundle")
+
+        myBundle?.extendedIngredients.let {
+            if (it != null) {
+                ingredientsAdapter.setData(it)
+            }
+        }
 
         return view
     }
 
+    fun setupRv(view:View){
+        binding.ingredientsRecyclerview.layoutManager=LinearLayoutManager(requireContext())
+        binding.ingredientsRecyclerview.adapter=ingredientsAdapter
+    }
 
 }
+
+
