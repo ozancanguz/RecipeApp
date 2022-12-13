0f4b1a871ac7a018cc7da7ed7c3626f0dbec157b
Ozan <ozancangu24@gmail.com>
2022-12-13 16:12:15 +0300

    -remote data source created

--- app/src/main/java/com/ozancanguz/recipeapp/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/data/remote/RemoteDataSource.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.recipeapp.data.remote
 
+import com.ozancanguz.recipeapp.data.models.FoodJoke
 import com.ozancanguz.recipeapp.data.models.FoodRecipe
 import com.ozancanguz.recipeapp.data.network.FoodRecipeApi
 import retrofit2.Response
@@ -14,6 +15,11 @@ class RemoteDataSource@Inject constructor(private val foodRecipeApi: FoodRecipeA
 
     suspend fun searchRecipes(searchQuery: Map<String, String>): Response<FoodRecipe> {
         return foodRecipeApi.searchRecipes(searchQuery)
+    }
+
+    suspend fun getFoodJoke(apikey:String):Response<FoodJoke>{
+        return  foodRecipeApi.getFoodJokes(apikey)
+
     }
 
 }
