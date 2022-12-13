4eddd12290c8dd683af98cb93b0c0a391815ebd6
Ozan <ozancangu24@gmail.com>
2022-12-13 16:29:38 +0300

    -recipe database modified

--- app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeDatabase.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/data/models/db/RecipeDatabase.kt
@@ -4,10 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.ozancanguz.recipeapp.data.models.db.entities.FavoriteEntity
+import com.ozancanguz.recipeapp.data.models.db.entities.FoodJokeEntity
 import com.ozancanguz.recipeapp.data.models.db.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class, FavoriteEntity::class],
+    entities = [RecipesEntity::class, FavoriteEntity::class,FoodJokeEntity::class],
     version = 1,
     exportSchema = false
 )
