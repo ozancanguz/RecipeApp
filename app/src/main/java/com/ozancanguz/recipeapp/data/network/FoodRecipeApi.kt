57912e372ad700af23f58ffb3538c10a71b9847c
Ozan <ozancangu24@gmail.com>
2022-12-13 16:11:05 +0300

    -foodjoke request created

--- app/src/main/java/com/ozancanguz/recipeapp/data/network/FoodRecipeApi.kt
+++ app/src/main/java/com/ozancanguz/recipeapp/data/network/FoodRecipeApi.kt
@@ -1,8 +1,10 @@
 package com.ozancanguz.recipeapp.data.network
 
+import com.ozancanguz.recipeapp.data.models.FoodJoke
 import com.ozancanguz.recipeapp.data.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipeApi {
@@ -17,6 +19,13 @@ interface FoodRecipeApi {
     suspend fun searchRecipes(
         @QueryMap searchQuery: Map<String, String>
     ): Response<FoodRecipe>
+
+
+    @GET("food/jokes/random")
+    suspend fun getFoodJokes(
+        @Query("apiKey") apiKey:String
+    ):Response<FoodJoke>
+
 
 
 
